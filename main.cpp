689bb302f9c4763af76a5108cd412ab0b55f5d20
kaekae15 <117469426+kaekae15@users.noreply.github.com>
2022-11-17 11:39:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,63 @@
-// write your code here
+//Kaylen Bolden          JNUMBER:J00965280          DATE:11/15/2022
+//                       PG.278 Example 5-4
+//                      DUE DATE: 11/16/2022
+#include <iostream>
+#include <string> 
+#include <iomanip>
+
+using namespace std;
+
+const string SENTINEL = "-1";
+
+int main()
+{
+    string name;
+    int numOfVolunteers;
+    int numOfBoxesSold;
+    int totalNumOfBoxesSold;
+    double costOfOneBox;
+
+    cout << fixed << showpoint << setprecision(2);
+
+    cout << "Line 14: Enter each volunteer's name and "
+         << "the number of boxes " << endl
+         <<"          sold by each volunteer, ending "
+         <<"with -1: " << endl; 
+
+    totalNumOfBoxesSold = 0;
+    numOfVolunteers = 0;
+
+    cin >> name; 
+
+    while (name != SENTINEL)
+    {
+        cin >> numOfBoxesSold;
+        totalNumOfBoxesSold = totalNumOfBoxesSold
+                              + numOfBoxesSold;
+        numOfVolunteers++;
+        cin >> name;
+    }
+    
+    cout << endl;
+
+    cout << "Line 26: The total number of boxes sold: "
+         << totalNumOfBoxesSold << endl;
+
+    cout << "Line 27: Enter the cost one box: ";
+    cin >> costOfOneBox;
+    cout << endl;
+
+    cout << "Line 30: The total money made by selling "
+         << "cookies: $"
+         << totalNumOfBoxesSold * costOfOneBox << endl;
+
+    if (numOfVolunteers !=0)
+       cout << "Line 32: The average number of "
+            << "boxes sold by each volunteer: "
+            << totalNumOfBoxesSold / numOfVolunteers
+            << endl;
+    else
+        cout << "Line 34: No input." << endl;
+
+    return 0;
+}
